cb9c3c05ab37dd2a67cb54e92ea73ce79d5455b0
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-01-30 19:29:10 +0000

    added hourly uodate function, now saving text when refreshing too

--- Assets/JS/JavaS.js
+++ Assets/JS/JavaS.js
@@ -30,4 +30,25 @@ $(document).ready(function() {
             };
         };
     };
-   
+    hourUpdate();
+    let interval = setInterval(hourUpdate, 15000);
+    $("#hour-0 .description").val(localStorage.getItem("hour-0"));
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+    $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+    $("#hour-6 .description").val(localStorage.getItem("hour-6"));
+    $("#hour-7 .description").val(localStorage.getItem("hour-7"));
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+    $("#hour-09 .description").val(localStorage.getItem("hour-09"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#currentDay").text(moment().format("dddd, MMMM, do"));
+});
